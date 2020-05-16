abf133b8af143219aaccdddd2b93503eb20b6a28
Fitsum <fitbez@gmail.com>
2020-05-16 10:49:03 -0400

    let and const

---
+++ index.js
@@ -0,0 +1,43 @@
+/* 1. var, let and const * /
+
+/* Old Version of JavaScript*/
+
+var name = "fitsum";
+
+var name = "helen";
+
+name = "adil";
+
+// console.log(name);
+
+/* talk about about scoping */
+
+var num = 5;
+
+function number() {
+  var num2 = 8;
+  console.log(num2);
+}
+
+// number();
+// console.log(num2);
+// console.log(num);
+
+/* ES6 */
+// let and const
+const birthYear = 2020;
+
+// birthYear = 1989;
+
+// console.log(birthYear);
+
+/* talk about scoping here */
+
+const list = [1, 3, 4, 5, 6];
+for (var i = 0; i < list.length; i++) {
+  // console.log(i);
+}
+
+console.log(i);
+
+2; /* template literal */
---
+++ random.js
@@ -0,0 +1,4 @@
+const students = ["Fitsum", "helen", "fikre", "beza", "temesgen", "adil"];
+let randomName = Math.floor(Math.random() * students.length);
+
+console.log(students[randomName]);
